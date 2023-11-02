d8bd2065442d1e5bd7437f7ed711971a59b8a74f
Itsmeambro <alanbiju8137@gmail.com>
2023-11-02 12:51:19 +0300

    first commit for test automation

---
+++ Jenkinsfile
@@ -0,0 +1,31 @@
+pipeline {
+    agent any
+    
+    stages {
+        stage('Checkout') {
+            steps {
+                git 'https://github.com/your-username/your-spring-boot-app.git'
+            }
+        }
+        
+        stage('Build') {
+            steps {
+                sh 'mvn clean package'
+            }
+        }
+        
+        // stage('Test') {
+        //     steps {
+        //         sh 'mvn test' // Adjust the test command as per your testing framework
+        //     }
+        // }
+        
+        stage('Deploy') {
+            steps {
+                sh 'cp target/your-app.war /path/to/tomcat/webapps'
+                sh '/path/to/tomcat/bin/shutdown.sh'
+                sh '/path/to/tomcat/bin/startup.sh'
+            }
+        }
+    }
+}
---
+++ src/main/java/com/alfaris/thymeleaf/SampleController.java
@@ -0,0 +1,37 @@
+package com.alfaris.thymeleaf;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Mono;
+
+@Controller
+@RestController
+@RequestMapping
+public class SampleController {
+
+	
+	@GetMapping("/")
+	String getDet() throws IOException {
+		
+		
+		return "Hello";
+	}
+	
+	@GetMapping("/date")
+	String getDete() throws IOException {
+		SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		Date date =  new Date();
+		String dateStr = format.format(date);
+		return dateStr;
+	}
+
+}
